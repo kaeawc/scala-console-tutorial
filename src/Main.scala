1260fba0a32f466553776c1a35f57f0ff7e5452f
Jason Pearson <jason.d.pearson@gmail.com>
2013-08-08 19:16:08 -0600

    added looping on user input

--- src/Main.scala
+++ src/Main.scala
@@ -4,8 +4,22 @@ object Main {
   def main(args: Array[String]) {
 
     val list = args mkString ", "
+    
+    val prompt = "Do you want to do something? (y/n)"
 
     println("Here are my args: " + list)
 
+    println(prompt)
+
+    while (Console.readLine() != "n") {
+
+      println("Doing something...")
+
+      println(prompt)
+
+    }
+
+
   }
+
 }
